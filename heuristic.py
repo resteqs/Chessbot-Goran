207222c43d7c9c66207f47fa26add48c4e0124ee
= <=>
2024-03-07 14:01:46 +0100

    small update

---
+++ heuristic.py
@@ -0,0 +1,22 @@
+#heuristic funtion
+
+class HeuristicFunction:
+    def __init__(self):
+
+        #init magnitudes, magX
+
+
+    
+    #heuristic megafunction
+    
+    def h_mega(self, s):
+        h = (mag1 * self.h_sub1(s) + mag2 * self.h_sub2(s) + ... + magX * self.h_subX(s)) / X
+        return h
+    
+
+    #heuristic subfunctions
+
+    def h_sub1(self, s):
+        return
+    
+    #etc
