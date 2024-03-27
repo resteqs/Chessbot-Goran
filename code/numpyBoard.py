1c9a8c00818a51cf26c0a012178c32427de309a8
= <=>
2024-03-27 14:41:38 +0100

    numpyBoard.py fully reworked now

--- code/numpyBoard.py
+++ code/numpyBoard.py
@@ -2,13 +2,13 @@
 # board class reworked as a numpy array
 
 import numpy as np
+
 import sys
-from pympler.asizeof import asizeof
+from pympler import asizeof as pym
 
 
 # indexing:
-# P, B, N, R, Q, K, same with black, turn color, castling, en passant
-
+# wP, wB, wN, wR, wQ, wK, bP, bB, bN, bR, bQ, bK, turn color, castling, en passant
 def createBoard():
     return np.empty(13, dtype=np.uint64)
 
@@ -16,16 +16,15 @@ def createBoard():
 def boardPrinting(bitboard):
     for bit in range(64, 0, -1):  # iterate from 64 (inclusive) to 0 (exclusive) with step -1
         if bit % 8 == 0:
-                print("")
-            # performs bitshifting on the bitboard and checks wheter LSB is 1 or 0. & is a binary AND operator 1&1 = 1, 0&1 = 0
-        curr_bit = (bitboard >> bit-1) & 1
-        if (curr_bit == 1):
-            print(curr_bit, end=" ")
+            print("")
+        # performs bitshifting on the bitboard and checks wheter LSB is 1 or 0. & is a binary AND operator 1&1 = 1, 0&1 = 0
+        curr_bit = (bitboard >> np.uint64(bit-1)) & np.uint64(1)
+        if (curr_bit == np.uint64(1)):
+            print("1", end=" ")
         else:
             print("·", end=" ")
 
 # prints the entire chess board
-#! currently uses typecasts so bitshift works - not so clean
 def entireBoardPrinting(board):
     entireBoard = board[0] | board[1] | board[2] | board[3] | board[4] | board[5] | board[6] | board[7] | board[8] | board[9] | board[10] | board[11]
     for bit in range(64, 0, -1):
@@ -59,30 +58,24 @@ def entireBoardPrinting(board):
                 print("k", end=" ")
         else:
             print("·", end=" ")
+    
+# returns all white pieces in one bitboard, making pieces undistinguishable
+def getWhitepieces(board):
+    return board[0] ^ board[1] ^ board[2] ^ board[3] ^ board[4] ^ board[5]
+    
+# returns all black pieces in one bitboard, making pieces undistinguishable
+def getBlackpieces(board):
+    return board[6] ^ board[7] ^ board[8] ^ board[9] ^ board[10] ^ board[11]
+    
+# switches turn color
+def switchTurn(board):
+    if board[12] == np.uint64(1):
+        board[12] = np.uint64(0)
+    else:
+        board[12] = np.uint64(1)
+
 
 """
-    # returns all bitboards in a list
-    def getBoard(self):
-        all_bitboards = [self.WHITE_PAWNS, self.WHITE_BISHOPS, self.WHITE_KNIGHTS, self.WHITE_ROOKS, self.WHITE_QUEEN, self.WHITE_KING,
-                         self.BLACK_PAWNS, self.BLACK_BISHOPS, self.BLACK_KNIGHTS, self.BLACK_ROOKS, self.BLACK_QUEEN, self.BLACK_KING]
-        return all_bitboards
-    
-    # returns all white pieces in one bitboard, making pieces undistinguishable
-    def getWhitepieces(self):
-        return self.WHITE_PAWNS ^ self.WHITE_BISHOPS ^ self.WHITE_KNIGHTS ^ self.WHITE_ROOKS ^ self.WHITE_QUEEN ^ self.WHITE_KING
-    
-    # returns all black pieces in one bitboard, making pieces undistinguishable
-    def getBlackpieces(self):
-        return self.BLACK_PAWNS ^ self.BLACK_BISHOPS ^ self.BLACK_KNIGHTS ^ self.BLACK_ROOKS ^ self.BLACK_QUEEN ^ self.BLACK_KING
-    
-    # switches turn color
-    def switchTurn(self):
-        if self.turn_Color == self.c_WHITE:
-            self.turn_Color = self.c_BLACK
-        else:
-            self.turn_Color = self.c_WHITE
-"""
-
 board = createBoard()
 board[0] = 0b0000000000000000000000000000000000000000000000001111111100000000
 board[1] = 0b0000000000000000000000000000000000000000000000000000000000100100
@@ -91,15 +84,24 @@ board[3] = 0b0000000000000000000000000000000000000000000000000000000010000001
 board[4] = 0b0000000000000000000000000000000000000000000000000000000000010000
 board[5] = 0b0000000000000000000000000000000000000000000000000000000000001000
 board[6] = 0b0000000011111111000000000000000000000000000000000000000000000000
-board[9] = 0b1000000100000000000000000000000000000000000000000000000000000000
-board[8] = 0b0100001000000000000000000000000000000000000000000000000000000000
 board[7] = 0b0010010000000000000000000000000000000000000000000000000000000000
+board[8] = 0b0100001000000000000000000000000000000000000000000000000000000000
+board[9] = 0b1000000100000000000000000000000000000000000000000000000000000000
 board[10] = 0b0001000000000000000000000000000000000000000000000000000000000000
 board[11] = 0b0000100000000000000000000000000000000000000000000000000000000000
+board[12] = 1
+
 print(board)
+print(board.dtype)
 entireBoardPrinting(board)
-"""
+boardPrinting(board[0])
+boardPrinting(getBlackpieces(board))
+boardPrinting(getWhitepieces(board))
+print(board[12])
+switchTurn(board)
+print(board[12])
+
 print(f"board size sys: {sys.getsizeof(board)}")
-print(f"board size pym: {asizeof(board)}")
+print(f"board size pym: {pym.asizeof(board)}")
 print(f"board size np: {board.nbytes}")
 """
